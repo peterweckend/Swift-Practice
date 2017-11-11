c7ff4c9d2aec3c0e80735d82fed1bb748ddf29d5
PeterWeckend <pweckend@ualberta.ca>
2017-11-11 15:39:45 -0700

    non-MVC basic screen created

---
+++ TabbedTest/TabbedTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TabbedTest
+//
+//  Created by Peter Weckend on 2017-11-11.
+//  Copyright © 2017 Peter Weckend. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
